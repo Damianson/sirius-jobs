d1c344c1da399403805532a07e491b630f9bc2bb
Damianson <damiansonuchegbu2017@gmail.com>
2022-11-29 15:14:43 +0100

    update

--- logic.js
+++ logic.js
@@ -1,35 +1,10 @@
-var text = [
-    "Web Development", 
-    "Fitness Coach", 
-    "Cleaning Service",
-    "Game Rental",
-    "Lesson Teachers",
-    "UI/UX",
-    "Article Writing",
-    "Therapy",
-    "Proof-reading",
-    "Architecture",
-    "Data Analysis",
-    "Transcript Collection",
-    "Painting",
-    "Event Planning",
-    "Lash Technician",
-    "Surprise Packages"
-];
-var counter = 0;
-var elem = $("#jobs");
-setInterval(change, 2000);
-function change() {
-    elem.fadeOut(function(){
-        elem.html(text[counter]);
-        counter++;
-        if(counter >= text.length) { counter = 0; }
-        elem.fadeIn();
-    });
-}
-
-
 $(document).scroll(function() {
     var scroll = $(window).scrollTop();
     $("body").css("background-position", "0" + (scroll / -1.8) + "px");
+
+    if (scroll > 150) {
+        $("nav").css("background-color", "#000000");
+    } else {
+        $("nav").css("background-color", "transparent");
+    }
   });
