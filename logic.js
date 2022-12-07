89a798eacfe6dce12c7ee2bec24f900ef92e77f2
Damianson <damiansonuchegbu2017@gmail.com>
2022-12-07 22:22:41 +0100

    update to website

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 $(document).scroll(function() {
     var scroll = $(window).scrollTop();
     $("body").css("background-position", "0" + (scroll / -1.8) + "px");
+    $(".hero-sec").css("opacity", (100 - scroll)/100 );
 
     if (scroll > 150) {
         $("nav").css("background-color", "#000000");
